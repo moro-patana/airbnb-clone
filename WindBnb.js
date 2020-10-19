fe86225556117c876778af3cf525b6c2d87ff1ae
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-10-19 16:11:55 +0300

    import airBnbList from the stays.js file

--- WindBnb.js
+++ WindBnb.js
@@ -1,2 +1,11 @@
 import React from "react"
+import airbnbList from "./stays.json"
+function WindBnb() {
+console.log(airbnbList);
+    return (
+          <div>
+              <h2>Hello</h2>
+          </div>
+    )
+}
 export default WindBnb;
