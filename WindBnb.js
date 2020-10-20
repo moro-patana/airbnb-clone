a02435aacf47e66b2da678714f464cb3f7a161c2
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-10-20 08:37:33 +0300

    wrapped the article in a div class container

--- WindBnb.js
+++ WindBnb.js
@@ -3,17 +3,26 @@ import airbnbList from "./stays.json"
 function WindBnb() {
 console.log(airbnbList);
     return (
-          <article>
+          <div className="airbnb-list">
               <header><h2>Stays in Finland</h2></header>
-              {airbnbList.map(bnb => (
-                        <ul>
-                            <li><img src={bnb.photo} /></li>
-                            <li>{bnb.superHost ? "superHost" : ""}</li>
-                            <li>{bnb.type}</li>
-                            <li>{bnb.rating}</li>
-                        </ul>
-              ))}
-          </article>
+
+              <div class="container">
+                  {airbnbList.map(bnb => (
+    
+                      <article key={bnb.id} className="article">
+                            <ul>
+                                <li><img src={bnb.photo} /></li>
+                                <li>{bnb.superHost ? "superHost" : ""}</li>
+                                <li>{bnb.type}</li>
+                                 {bnb.beds ? <li>{bnb.beds} bed(s)</li> : ""}
+                                <li>{bnb.rating}</li>
+                            </ul>
+                            <p>{bnb.title}</p>
+                            <p>{bnb.city}</p>
+                      </article>
+                  ))}
+              </div>
+          </div>
     )
 }
 export default WindBnb;
