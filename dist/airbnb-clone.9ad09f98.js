ed552df467bdba8d203e200baea33083acc8e1af
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-10-20 10:30:17 +0300

    put position relative to the ul

--- dist/airbnb-clone.9ad09f98.js
+++ dist/airbnb-clone.9ad09f98.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./star-icon.svg":[["star-icon.7c92ff19.svg","star-icon.svg"],"star-icon.svg"],"_css_loader":"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53413" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59169" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
