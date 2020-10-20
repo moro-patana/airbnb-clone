643ab40debbc4d5753c09406c02a281ff39d1ed6
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-10-20 14:53:44 +0300

    called the searchLocation function

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import React from "react"
 import WindBnb from "./WindBnb.js"
+import airBnbList from "./stays.json"
+import SearchLocation from "./SearchLocation.js"
 function App() {
     return (
-        <WindBnb />
+        < SearchLocation
+        />
     )
 }
 export default App;
